bf7c52d7e4c4a7169f45ad949746439c5d42659d
Manuel Eberle <>
2023-04-22 16:57:06 +0200

    init octokit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const core = require('@actions/core');
-core.summary.addHeading('Test Results').write()
+
+core.summary
+    .addHeading('Build summary')
+    .addRaw('this is a description of the build')
+    .addTable([
+        [{data: 'Test', header: true}, {data: 'Result', header: true}],
+        ['a1', 'Pass'],
+        ['a2', 'Failed']])
+    .addList(['<a href="http://example.com/" title="Title">an example</a>'])
+    .addLink('View staging deployment!', 'https://github.com')
+    .write()
